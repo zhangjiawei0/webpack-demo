a223fb12b0258fae04f7e05af647c88e77b3c937
zhangjiawei0 <15231120650@sina.cn>
2017-07-13 15:11:08 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
 	var element = document.createElement('div');
+	var btn = document.createElement('button');
 
 	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -13,10 +15,13 @@ function component() {
 	// Add the image to our existing div.
 	var myIcon = new Image();
 	myIcon.src = Icon;
-
 	element.appendChild(myIcon);
 
 	console.log(Data);
+
+	btn.innerHTML = 'Click me!';
+	btn.onclick = printMe;
+	element.appendChild(btn);
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js',
+		vendor: ['lodash']
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
